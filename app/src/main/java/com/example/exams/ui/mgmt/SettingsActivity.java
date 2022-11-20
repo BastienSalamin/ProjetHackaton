ce83fbe16e7751bb1fb5555158c704891849f12f
Bastien Salamin <bastien.salamin@students.hevs.ch>
2022-11-20 13:37:43 +0100

    Insertion lien GitHub dans À propos

--- app/src/main/java/com/example/exams/ui/mgmt/SettingsActivity.java
+++ app/src/main/java/com/example/exams/ui/mgmt/SettingsActivity.java
@@ -3,6 +3,8 @@ package com.example.exams.ui.mgmt;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.text.method.LinkMovementMethod;
+import android.widget.TextView;
 
 import com.example.exams.R;
 
@@ -12,5 +14,8 @@ public class SettingsActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_settings);
+
+        TextView textView = findViewById(R.id.textView);
+        textView.setMovementMethod(LinkMovementMethod.getInstance());
     }
 }
