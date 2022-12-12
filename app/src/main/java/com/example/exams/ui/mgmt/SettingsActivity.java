133e189ae7df33a074fe56187556e07156d0cbff
Bastien Salamin <bastien.salamin@students.hevs.ch>
2022-12-12 20:42:33 +0100

    Implémentation d'un dark mode

--- app/src/main/java/com/example/exams/ui/mgmt/SettingsActivity.java
+++ app/src/main/java/com/example/exams/ui/mgmt/SettingsActivity.java
@@ -1,14 +1,20 @@
 package com.example.exams.ui.mgmt;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatDelegate;
 
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.text.method.LinkMovementMethod;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Switch;
 import android.widget.TextView;
 
 import com.example.exams.R;
 
 public class SettingsActivity extends AppCompatActivity {
+    private Switch btnToggleDark;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,5 +23,34 @@ public class SettingsActivity extends AppCompatActivity {
 
         TextView textView = findViewById(R.id.textView);
         textView.setMovementMethod(LinkMovementMethod.getInstance());
+
+        btnToggleDark = findViewById(R.id.switchDark);
+
+        SharedPreferences sharedPreferences = getSharedPreferences("sharedPrefs", MODE_PRIVATE);
+        final SharedPreferences.Editor editor = sharedPreferences.edit();
+        final boolean isDarkModeOn = sharedPreferences.getBoolean("isDarkModeOn", false);
+
+        if (isDarkModeOn) {
+            btnToggleDark.setChecked(true);
+        }
+        else {
+            btnToggleDark.setChecked(false);
+        }
+
+        btnToggleDark.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View view) {
+                if (isDarkModeOn) {
+                    AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO);
+                    editor.putBoolean("isDarkModeOn", false);
+                    editor.apply();
+                } else {
+                    AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES);
+                    editor.putBoolean("isDarkModeOn", true);
+                    editor.apply();
+                }
+            }
+        });
     }
 }
